46e181e8d4decc4d7321b52b60d079ea67dbb9c6
samuel <samuelkelvinmwangi@gmail.com>
2023-08-31 09:10:34 +0300

    adding corrections to the migration file

--- db/migrate/20230831054347_create_campers.rb
+++ db/migrate/20230831054347_create_campers.rb
@@ -1,6 +1,8 @@
 class CreateCampers < ActiveRecord::Migration[6.1]
   def change
     create_table :campers do |t|
+      t.string :name
+      t.integer :age
 
       t.timestamps
     end
--- db/migrate/20230831054559_create_signups.rb
+++ db/migrate/20230831054559_create_signups.rb
@@ -1,7 +1,10 @@
 class CreateSignups < ActiveRecord::Migration[6.1]
   def change
     create_table :signups do |t|
-
+      t.integer :time
+      t.references :camper, foreign_key: true
+      t.references :activity, foreign_key: true
+      
       t.timestamps
     end
   end
