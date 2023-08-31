86483fd39d841efb0cf3d9aa9c8215bf3e299e28
samuel <samuelkelvinmwangi@gmail.com>
2023-08-31 08:50:53 +0300

    adding data to the route page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :campers, only: [:index, :show, :create]
+  resources :activities, only: [:index, :destroy]
+  resources :signups, only: [:create]
+
+  get '/campers/:id', to: 'campers#show_with_activities'
 end
